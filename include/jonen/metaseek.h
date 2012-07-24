65522f19cd1e09d9739fd94d0da31ec958bc2e2b
Geoffrey Biggs <git@killbots.net>
2012-07-24 23:55:16 +0900

    Metaseek element interface

--- include/jonen/metaseek.h
+++ include/jonen/metaseek.h
@@ -40,6 +40,7 @@
 #define METASEEK_H_
 
 #include <jonen/element_base.h>
+#include <jonen/jonen_config.h>
 #include <jonen/master_element.h>
 #include <jonen/win_dll.h>
 
@@ -67,16 +68,63 @@ namespace jonen
      * This class implements the Element interface.
      *
      * This class implements the MasterElement interface.
+     *
+     * This class implements the std::multimap interface.
      */
     class JONEN_EXPORT Metaseek :
         public ElementBase<Metaseek>,
         public MasterElement,
-        boost::equality_comparable<Metaseek>
+        boost::equality_comparable<Metaseek>,
+        boost::less_than_comparable<Metaseek>
     {
         friend class ElementBase<Metaseek>;
 
         public:
-            /// \brief Constructor.
+            /// \brief The key type (Key) of this container.
+            typedef ids::ID key_type;
+            /// \brief The mapped type (T) of this container.
+            typedef std::streamoff mapped_type;
+
+        private:
+            /// \brief The type of the internal storage.
+            typedef std::multimap<key_type, mapped_type> storage_type_;
+
+        public:
+            // Export typedefs
+            /// \brief The value type of this container.
+            typedef storage_type_::value_type value_type;
+            /// \brief The size type of this container.
+            typedef storage_type_::size_type size_type;
+            /// \brief The type used for differences.
+            typedef storage_type_::difference_type difference_type;
+            /// \brief Comparator.
+            typedef storage_type_::key_compare key_compare;
+            /// \brief The allocator type.
+            typedef storage_type_::allocator_type allocator_type;
+            /// \brief The reference type.
+            typedef storage_type_::reference reference;
+            /// \brief The constant reference type.
+            typedef storage_type_::const_reference const_reference;
+            /// \brief The pointer type.
+            typedef storage_type_::pointer pointer;
+            /// \brief The constant pointer type.
+            typedef storage_type_::const_pointer const_pointer;
+            /// \brief The random access iterator type.
+            typedef storage_type_::iterator iterator;
+            /// \brief The constant random access iterator type.
+            typedef storage_type_::const_iterator const_iterator;
+            /// \brief The reversed random access iterator type.
+            typedef storage_type_::reverse_iterator reverse_iterator;
+            /// \brief The constant reversed random access iterator type.
+            typedef storage_type_::const_reverse_iterator
+                const_reverse_iterator;
+            /// \brief The class that compares two entries.
+            typedef storage_type_::value_compare value_compare;
+
+            /** \brief Constructor.
+             *
+             * Upon creation, the index will be empty.
+             */
             Metaseek();
 
             /// \brief Copy constructor.
@@ -88,14 +136,200 @@ namespace jonen
             /// \brief Destructor.
             ~Metaseek();
 
+            /// \brief Get the allocator.
+            allocator_type get_allocator() const;
+
+            /// \brief Get an iterator to the first index entry.
+            iterator begin();
+            /// \brief Get an iterator to the first index entry.
+            const_iterator begin() const;
+#if defined(JONEN_CPLUSPLUS11_SUPPORT)
+            /// \brief Get an iterator to the first index entry.
+            const_iterator cbegin() const;
+#endif // defined(JONEN_CPLUSPLUS11_SUPPORT)
+            /** \brief Get an iterator to the position past the last index
+             * entry.
+             */
+            iterator end();
+            /** \brief Get an iterator to the position past the last index
+             * entry.
+             */
+            const_iterator end() const;
+#if defined(JONEN_CPLUSPLUS11_SUPPORT)
+            /** \brief Get an iterator to the position past the last index
+             * entry.
+             */
+            const_iterator cend() const;
+#endif // defined(JONEN_CPLUSPLUS11_SUPPORT)
+            /// \brief Get a reverse iterator to the last index entry.
+            reverse_iterator rbegin();
+            /// \brief Get a reverse iterator to the last index entry.
+            const_reverse_iterator rbegin() const;
+#if defined(JONEN_CPLUSPLUS11_SUPPORT)
+            /// \brief Get a reverse iterator to the last index entry.
+            const_reverse_iterator crbegin() const;
+#endif // defined(JONEN_CPLUSPLUS11_SUPPORT)
+            /** \brief Get a reverse iterator to the position before the first
+             * index entry.
+             */
+            reverse_iterator rend();
+            /** \brief Get a reverse iterator to the position before the first
+             * index entry.
+             */
+            const_reverse_iterator rend() const;
+#if defined(JONEN_CPLUSPLUS11_SUPPORT)
+            /** \brief Get a reverse iterator to the position before the first
+             * index entry.
+             */
+            const_reverse_iterator crend() const;
+#endif // defined(JONEN_CPLUSPLUS11_SUPPORT)
+
+            /// \brief Check if there are no index entries.
+            bool empty() const;
+            /// \brief Get the number of index entries.
+            size_type size() const;
+            /// \brief Get the maximum number of index entries.
+            size_type max_size() const;
+
+            /// \brief Remove all index entries.
+            void clear();
+            /** \brief Insert a new index entry.
+             *
+             * If an index entry already exists with the same ID, the new
+             * offset is entered into the index after it. No index entries are
+             * overwritten.
+             *
+             * \param[in] value The offset to insert and its ID.
+             *
+             * \return The iterator at the position where the offset was added.
+             */
+            iterator insert(value_type const& value);
+            /** \brief Insert a range of offsets.
+             *
+             * \param[in] first The start of the range.
+             * \param[in] last The end of the range.
+             */
+            void insert(const_iterator first, const_iterator last);
+#if defined(JONEN_CPLUSPLUS11_SUPPORT)
+            /** \brief Erase the index entry at the specified iterator.
+             *
+             * \param[in] position The position to erase at.
+             * \return An iterator following the removed element.
+             */
+            iterator erase(const_iterator position);
+#else // defined(JONEN_CPLUSPLUS11_SUPPORT)
+            /** \brief Erase the index entry at the specified iterator.
+             *
+             * \param[in] position The position to erase at.
+             */
+            void erase(iterator position);
+#endif // defined(JONEN_CPLUSPLUS11_SUPPORT)
+#if defined(JONEN_CPLUSPLUS11_SUPPORT)
+            /** \brief Erase a range of index entries.
+             *
+             * \param[in] first The start of the range.
+             * \param[in] last The end of the range.
+             * \return An iterator following the last removed element.
+             */
+            iterator erase(const_iterator first, const_iterator last);
+#else // defined(JONEN_CPLUSPLUS11_SUPPORT)
+            /** \brief Erase a range of index entries.
+             *
+             * \param[in] first The start of the range.
+             * \param[in] last The end of the range.
+             */
+            void erase(iterator first, iterator last);
+#endif // defined(JONEN_CPLUSPLUS11_SUPPORT)
+            /** \brief Erase all index entries with the given ID.
+             *
+             * \param[in] id The ID to erase.
+             * \return The number of entries erased.
+             */
+            size_type erase(key_type const& id);
             /** \brief Swap this element with another.
              *
              * \param[in] other The other element to swap with.
              */
             void swap(Metaseek& other);
 
-            /// \brief Equality operator
+            /** \brief Get the number of elements with the given key.
+             *
+             * \param[in] id The ID to search for.
+             */
+            size_type count(key_type const& id) const;
+            /** \brief Search for the index entry with the given ID.
+             *
+             * If multiple offsets exist for the ID, the first one inserted
+             * will be returned.
+             *
+             * \param[in] id The ID to search for.
+             * \return An iterator to the matching offset, or end() if
+             * there is no index entry with that ID.
+             */
+            iterator find(key_type const& id);
+            /** \brief Search for the index entry with the given ID.
+             *
+             * \param[in] id The ID to search for.
+             * \return An iterator to the matching offset, or end() if
+             * there is no index entry with that ID.
+             */
+            const_iterator find(key_type const& id) const;
+            /** \brief Get a range containing all elements with the given ID.
+             *
+             * The range is defined by two iterators. One points to the first
+             * element not less than key, and the other points to the first
+             * element greater than key.
+             *
+             * This method is equivalent to calling both lower_bound() and
+             * upper_bound().
+             *
+             * \param[in] id The ID to search for.
+             */
+            std::pair<iterator, iterator> equal_range(key_type const& id);
+            /** \brief Get a range containing all elements with the given ID.
+             *
+             * The range is defined by two iterators. One points to the first
+             * element not less than key, and the other points to the first
+             * element greater than key.
+             *
+             * This method is equivalent to calling both lower_bound() and
+             * upper_bound().
+             *
+             * \param[in] id The ID to search for.
+             */
+            std::pair<const_iterator, const_iterator> equal_range(
+                    key_type const& id) const;
+            /** \brief Find the first element not less than the given ID.
+             *
+             * \param[in] id The ID to search for.
+             */
+            iterator lower_bound(key_type const& id);
+            /** \brief Find the first element not less than the given ID.
+             *
+             * \param[in] id The ID to search for.
+             */
+            const_iterator lower_bound(key_type const& id) const;
+            /** \brief Find the first element greater than the given ID.
+             *
+             * \param[in] id The ID to search for.
+             */
+            iterator upper_bound(key_type const& id);
+            /** \brief Find the first element greater than the given ID.
+             *
+             * \param[in] id The ID to search for.
+             */
+            const_iterator upper_bound(key_type const& id) const;
+
+            /// \brief Get the function used to compare IDs.
+            key_compare key_comp() const;
+            /// \brief Get the function object used to compare ID-offset pairs.
+            value_compare value_comp() const;
+
+            /// \brief Equality operator.
             bool operator==(Metaseek const& rhs);
+
+            /// \brief Less-than operator.
+            bool operator<(Metaseek const& rhs);
 
         private:
             // Pimpl member
